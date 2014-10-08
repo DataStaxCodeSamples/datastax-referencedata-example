7474de43e2374d8e7e6c7fe852d1e9df7c2cca81
Patrick Callaghan <patcho@gmail.com>
2014-10-08 16:43:10 +0200

    updated wait

--- src/main/java/com/datastax/refdata/ReferenceDao.java
+++ src/main/java/com/datastax/refdata/ReferenceDao.java
@@ -20,13 +20,17 @@ public class ReferenceDao {
 	private static String keyspaceName = "datastax_referencedata_demo";
 	private static String tableNameHistoric = keyspaceName + ".historic_data";
 	private static String tableNameDividends = keyspaceName + ".dividends";
-	private static String tableNameMetaData = keyspaceName + ".exchange_metadata";
+	private static String tableNameMetaData = keyspaceName
+			+ ".exchange_metadata";
 
-	private static final String INSERT_INTO_HISTORIC = "Insert into " + tableNameHistoric
+	private static final String INSERT_INTO_HISTORIC = "Insert into "
+			+ tableNameHistoric
 			+ " (exchange,symbol,date,open,high,low,close,volume,adj_close) values (?,?,?,?,?,?,?,?,?);";
-	private static final String INSERT_INTO_DIVIDENDS = "Insert into " + tableNameDividends
+	private static final String INSERT_INTO_DIVIDENDS = "Insert into "
+			+ tableNameDividends
 			+ " (exchange,symbol,date,dividend) values (?,?,?,?);";
-	private static final String INSERT_INTO_METADATA = "Insert into " + tableNameMetaData
+	private static final String INSERT_INTO_METADATA = "Insert into "
+			+ tableNameMetaData
 			+ " (exchange,symbol,last_updated_date) values (?,?,?);";
 
 	private PreparedStatement insertStmtHistoric;
@@ -35,25 +39,26 @@ public class ReferenceDao {
 
 	public ReferenceDao(String[] contactPoints) {
 
-		Cluster cluster = Cluster.builder().addContactPoints(contactPoints).build();
+		Cluster cluster = Cluster.builder().addContactPoints(contactPoints)
+				.build();
 		this.session = cluster.connect();
 
 		this.insertStmtHistoric = session.prepare(INSERT_INTO_HISTORIC);
 		this.insertStmtDividend = session.prepare(INSERT_INTO_DIVIDENDS);
 		this.insertStmtMetaData = session.prepare(INSERT_INTO_METADATA);
-		
+
 		this.insertStmtHistoric.setConsistencyLevel(ConsistencyLevel.ONE);
 		this.insertStmtDividend.setConsistencyLevel(ConsistencyLevel.ONE);
 		this.insertStmtMetaData.setConsistencyLevel(ConsistencyLevel.ONE);
 	}
 
-	public void insertHistoricData(List<HistoricData> list) throws Exception{
+	public void insertHistoricData(List<HistoricData> list) throws Exception {
 		BoundStatement boundStmt = new BoundStatement(this.insertStmtHistoric);
 		List<ResultSetFuture> results = new ArrayList<ResultSetFuture>();
 
-		Date mostRecentDate = new Date(0);		
+		Date mostRecentDate = new Date(0);
 		HistoricData mostRecent = null;
-		
+
 		for (HistoricData historicData : list) {
 
 			boundStmt.setString("exchange", historicData.getExchange());
@@ -67,45 +72,37 @@ public class ReferenceDao {
 			boundStmt.setDouble("adj_close", historicData.getAdjClose());
 
 			results.add(session.executeAsync(boundStmt));
-			
-			if (historicData.getDate().after(mostRecentDate)){				
+
+			if (historicData.getDate().after(mostRecentDate)) {
 				mostRecentDate = historicData.getDate();
 				mostRecent = historicData;
 			}
-						
-			TOTAL_POINTS++;			
+
+			TOTAL_POINTS++;
 		}
 
-		//Insert most recent date.
-		if (mostRecent != null){
-			BoundStatement boundMetaDataStmt = new BoundStatement(this.insertStmtMetaData);
+		// Insert most recent date.
+		if (mostRecent != null) {
+			BoundStatement boundMetaDataStmt = new BoundStatement(
+					this.insertStmtMetaData);
 			boundMetaDataStmt.setString("exchange", mostRecent.getExchange());
 			boundMetaDataStmt.setString("symbol", mostRecent.getSymbol());
-			boundMetaDataStmt.setDate("last_updated_date", mostRecent.getDate());
+			boundMetaDataStmt
+					.setDate("last_updated_date", mostRecent.getDate());
 			results.add(session.executeAsync(boundMetaDataStmt));
 		}
-		
-		//Wait till we have everything back.
-		boolean wait = true;
-		while (wait) {
-			// start with getting out, if any results are not done, wait is
-			// true.
-			wait = false;
-			for (ResultSetFuture result : results) {
-				if (!result.isDone()) {
-					wait = true;
-					break;
-				}
-			}
+
+		// Wait till we have everything back.
+		for (ResultSetFuture future : results) {
+			future.getUninterruptibly();
 		}
-		return;
 	}
-	
+
 	public void insertDividend(List<Dividend> list) {
 		BoundStatement boundStmt = new BoundStatement(this.insertStmtDividend);
 		List<ResultSetFuture> results = new ArrayList<ResultSetFuture>();
 
-		for (Dividend dividend: list) {
+		for (Dividend dividend : list) {
 
 			boundStmt.setString("exchange", dividend.getExchange());
 			boundStmt.setString("symbol", dividend.getSymbol());
@@ -115,21 +112,15 @@ public class ReferenceDao {
 			results.add(session.executeAsync(boundStmt));
 		}
 
-		//Wait till we have everything back.
-		boolean wait = true;
-		while (wait) {
-			wait = false;
-			for (ResultSetFuture result : results) {
-				if (!result.isDone()) {
-					wait = true;
-					break;
-				}
-			}
+		// Wait till we have everything back.
+		for (ResultSetFuture future : results) {
+			future.getUninterruptibly();
 		}
+
 		return;
 	}
-	
-	public long getTotalPoints(){
+
+	public long getTotalPoints() {
 		return TOTAL_POINTS;
 	}
 }
